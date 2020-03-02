46070a0ccbc813b0bba800398da810f747bd34e8
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2020-03-02 03:41:53 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,9 @@ get '/admin/authors/delete', to: 'authors#delete'
 get '/admin/authors/create', to: 'authors#create'
 get '/admin/comments/moderate', to: 'comments#moderate'
 
+scope '/admin' do
+  resources :stats, only: [:index]
+end
+
   root 'posts#index'
 end
